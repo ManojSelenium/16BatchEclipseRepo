6f142aac276dc95f603034bb1dc6e85f76210478
ManojSelenium <manojqa5006@gmail.com>
2019-01-22 09:19:53 +0530

    Added New line

--- MavenFirstProject/src/test/java/com/maven/testcases/FirstTest.java
+++ MavenFirstProject/src/test/java/com/maven/testcases/FirstTest.java
@@ -1,5 +1,6 @@
 package com.maven.testcases;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -13,6 +14,7 @@ public class FirstTest {
 		WebDriver driver=new ChromeDriver();
 		
 		driver.get("http://www.google.co.in");
+		driver.findElement(By.name("q")).sendKeys("selenium");
 		driver.quit();
 	}
 }
